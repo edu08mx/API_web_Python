a7235fc9fc6766f183f2452fecad90171355d9c5
edu08mx <eduardo08mx@gmail.com>
2024-04-17 19:18:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
 usuarios = [
-    {"id": 1, "nome": "Usuario1", "cpf": "12345", "age": "10"},
-    {"id": 2, "nome": "Usuario2", "cpf": "123456", "age": "11"},
-    {"id": 3, "nome": "Usuario3", "cpf": "1234567", "age": "12"},
-    {"id": 4, "nome": "Usuario4", "cpf": "12345678", "age": "13"},
-    {"id": 5, "nome": "Usuario5", "cpf": "123456789", "age":"14"}
+    {"id": 1, "nome": "Usuario1", "cpf": "12345", "age": "1"},
+    {"id": 2, "nome": "Usuario2", "cpf": "123456", "age": "2"},
+    {"id": 3, "nome": "Usuario3", "cpf": "1234567", "age": "3"},
+    {"id": 4, "nome": "Usuario4", "cpf": "12345678", "age": "4"},
+    {"id": 5, "nome": "Usuario5", "cpf": "123456789", "age": "5"}
 ]
 
 @app.route('/cadastrarusuarios', methods=['POST'])
@@ -14,13 +15,16 @@ def cadastrar_usuario():
     dados = request.get_json()
     novo_id = dados.get('id')
     novo_nome = dados.get('nome')
+    novo_cpf = dados.get('cpf')
+    nova_age = dados.get('age')
+
 
     if novo_id and novo_nome:
         for usuario in usuarios:
             if usuario["id"] == novo_id:
                 return jsonify({"erro": "ID já existe"}), 400
 
-        novo_usuario = {"id": novo_id, "nome": novo_nome}
+        novo_usuario = {"id": novo_id, "nome": novo_nome, "cpf": novo_cpf, "age": nova_age}
         usuarios.append(novo_usuario)
         return jsonify({"mensagem": "Usuário cadastrado com sucesso!"}), 201
     else:
@@ -30,10 +34,15 @@ def cadastrar_usuario():
 def atualizar_usuario(id):
     dados = request.get_json()
     novo_nome = dados.get('nome')
+    novo_cpf = dados.get('cpf')
+    nova_age = dados.get('age')
 
     for usuario in usuarios:
         if usuario["id"] == id:
             usuario["nome"] = novo_nome
+            usuario["cpf"] = novo_cpf
+            usuario["age"] = nova_age
+
             return jsonify({"mensagem": "Usuário atualizado com sucesso!"}), 200
 
     return jsonify({"erro": "Usuário não encontrado"}), 404
@@ -59,11 +68,13 @@ def obter_usuario_por_id(id):
 def atualizar_dados_usuario(id):
     dados = request.get_json()
     novo_nome = dados.get('nome')
+    novo_cpf = dados.get('cpf')
 
     for usuario in usuarios:
         if usuario["id"] == id:
             if novo_nome:
                 usuario["nome"] = novo_nome
+                usuario["cpf"] = novo_cpf
             return jsonify({"mensagem": "Dados do usuário atualizados com sucesso!"}), 200
 
     return jsonify({"erro": "Usuário não encontrado"}), 404
